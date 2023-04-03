ca72425e320a4536da9f5403471bfab413fcefa4
Thomas8483 <thomas.jaxel@gmail.com>
2023-04-03 10:50:26 +0200

    Génération du CSR

--- static/createCSR.sh
+++ static/createCSR.sh
@@ -12,5 +12,5 @@ cn=$8
 # Génération de la paire de clé
 openssl genpkey -algorithm RSA -out $cn.key -aes256 -pass pass:"isen"
 
-# Use the form data to create the CSR using openssl req
-#openssl req -new -key $cn.key -out $cn.csr -subj "/C=$country/ST=$state/L=$city/O=$org/OU=$unit/CN=$cn" -passout pass:"isen"
+# Génération du CSR
+openssl req -new -key $cn.key -out $cn.csr -subj "/C=$country/ST=$state/L=$city/O=$org/OU=$unit/CN=$cn" -passin pass:"isen"
